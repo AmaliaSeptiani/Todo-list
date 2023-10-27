7e93073cbae58c1ca08de177a7b9c0739cebd6bd
AmaliaSeptiani <amalia2003septiani@students.unnes.ac.id>
2023-10-27 19:34:28 +0700

    initial commit

---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function InputTodo(){
+    return (
+        <div>
+        <form>
+        <input type="text" placeholder='input todo' />
+        <button>Add</button>
+        </form>
+
+        </div>
+        )  
+}
+export default InputTodo
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function TodoList() {
+  return (
+    <div>
+        <div>
+            <span>Belajar React</span>
+            <button>✏️</button>
+            <button>❌</button>
+        </div>
+        <div>
+            <span>Belajar React</span>
+            <button>✏️</button>
+            <button>❌</button>
+        </div>
+        <div>
+            <span>Belajar React</span>
+            <button>✏️</button>
+            <button>❌</button>
+        </div>
+    </div>
+  )
+}
+
+export default TodoList
