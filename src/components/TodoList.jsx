7d0b05722d03de8005f69ee5699227bac53a7494
AmaliaSeptiani <amalia2003septiani@students.unnes.ac.id>
2023-10-29 18:37:58 +0700

    redux

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,19 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { deleteTodo } from '../redux/reducers/todo-reducer';
 
 function TodoList() {
-    const {todos} = useSelector(state => state.todo)
+    const dispatch = useDispatch()
+    const {todos} = useSelector(state => state.todo);
+    const handleDelete = (id) => {
+        dispatch(deleteTodo(id))
+    }
   return (
     <div>
         {todos.map((todo) => (
         <div key={todo.id}>
             <span>{todo.value}</span>
             <button>✏️</button>
-            <button>❌</button>
+            <button onClick={() => handleDelete(todo.id)}>❌</button>
         </div>
         ))}
            </div>
